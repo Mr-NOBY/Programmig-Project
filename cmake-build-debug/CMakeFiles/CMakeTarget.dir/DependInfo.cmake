a72354c70159eb05f54c753fc624ea28233aaab3
Ahmed Essam <alasad.elshaky@gmail.com>
2024-04-27 22:16:02 +0300

    Uni Class

---
+++ cmake-build-debug/CMakeFiles/CMakeTarget.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ahmed Essam/CLionProjects/Programmig-Project/Programmig Project.cpp" "CMakeFiles/CMakeTarget.dir/Programmig_Project.cpp.obj" "gcc" "CMakeFiles/CMakeTarget.dir/Programmig_Project.cpp.obj.d"
+  "C:/Users/Ahmed Essam/CLionProjects/Programmig-Project/Uni.cpp" "CMakeFiles/CMakeTarget.dir/Uni.cpp.obj" "gcc" "CMakeFiles/CMakeTarget.dir/Uni.cpp.obj.d"
+  "C:/Users/Ahmed Essam/CLionProjects/Programmig-Project/User.cpp" "CMakeFiles/CMakeTarget.dir/User.cpp.obj" "gcc" "CMakeFiles/CMakeTarget.dir/User.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CMakeTarget.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeTarget.dir/Programmig_Project.cpp.obj"
+  "CMakeFiles/CMakeTarget.dir/Programmig_Project.cpp.obj.d"
+  "CMakeFiles/CMakeTarget.dir/Uni.cpp.obj"
+  "CMakeFiles/CMakeTarget.dir/Uni.cpp.obj.d"
+  "CMakeFiles/CMakeTarget.dir/User.cpp.obj"
+  "CMakeFiles/CMakeTarget.dir/User.cpp.obj.d"
+  "CMakeTarget.exe"
+  "CMakeTarget.exe.manifest"
+  "CMakeTarget.pdb"
+  "libCMakeTarget.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
